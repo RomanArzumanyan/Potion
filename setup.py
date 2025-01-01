d922c9620fcf83781fdd2645fe54a7bea4e071af
Roman Arzumanyan <RomanArzumanyan@users.noreply.github.com>
2025-01-01 17:07:54 +0300

    build to testpi and pypi (#10)
    Co-authored-by: Roman Arzumanyan <r.arzumanyan@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python_potion',
-    version='0.1.0-0',
+    version='0.1.0-1',
     description='Live video inference tool',
     author='Roman Arzumanyan',
     author_email='roman.arzum@gmail.com',

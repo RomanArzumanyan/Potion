a07abce4331bc442e0e20c8dded0185f03e5dead
Roman Arzumanyan <r.arzumanyan@gmail.com>
2025-01-01 17:13:06 +0300

    brush up pyproject and setup

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='python_potion',
     version='0.1.0-1',
     description='Live video inference tool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Roman Arzumanyan',
     author_email='roman.arzum@gmail.com',
     packages=['python_potion'],
